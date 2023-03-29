7d65f7609477e3c4fc22cd2bb82ed779b7984c3a
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-03-29 18:17:47 -0400

    bonus 1 complete

--- index.js
+++ index.js
@@ -51,12 +51,24 @@ if (hacker1.localeCompare(hacker2) === -1) {
 
 
 //Bonus 1
-let sentence = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare felis sit amet ipsum mollis, in congue enim sagittis. Mauris imperdiet porttitor magna rhoncus imperdiet. Nulla eget odio erat. Donec ultrices fringilla rutrum. Sed in pulvinar sapien. Ut ac neque sed neque placerat ultricies ut ut odio. Nullam dictum vehicula augue, non scelerisque leo pulvinar a. Etiam eget venenatis quam.
+let longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare felis sit amet ipsum mollis, in congue enim sagittis. Mauris imperdiet porttitor magna rhoncus imperdiet. Nulla eget odio erat. Donec ultrices fringilla rutrum. Sed in pulvinar sapien. Ut ac neque sed neque placerat ultricies ut ut odio. Nullam dictum vehicula augue, non scelerisque leo pulvinar a. Etiam eget venenatis quam.
 
 Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla at leo non purus pharetra blandit nec sed eros. Suspendisse commodo nisl eget ornare sagittis. Vivamus ipsum tortor, elementum dictum ipsum at, tempus iaculis magna. Proin id justo commodo, vulputate nisi non, varius nulla. Duis ac quam nisi. Phasellus vehicula, leo sit amet consectetur dictum, tortor lectus euismod justo, ut gravida dolor libero at ligula.
 
 Sed placerat et lectus eu finibus. Integer id quam facilisis, bibendum velit at, semper sem. Suspendisse ut diam ut augue tincidunt feugiat. Etiam in rhoncus diam. Vestibulum in luctus quam. In interdum turpis vel est suscipit malesuada. Aliquam erat volutpat. Ut quis ante et eros fermentum hendrerit.`
 
-let sentenceArray = sentence.replaceAll('\n\n', ' ');
-
+let sentenceArray = longText.replaceAll('\n\n', ' ')
 console.log(sentenceArray.split(' ').length);
+
+
+let etCountArr;
+etCountArr = longText.toLowerCase().replaceAll('.', '').replaceAll(',', '').split(' ');
+
+let wordCount = 0;
+for (i=0; i<etCountArr.length; i++) {
+    if (etCountArr[i] === 'et') {
+        wordCount++;
+    }
+}
+
+console.log(wordCount);
